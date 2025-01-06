e909cbe5af6bdf09884cae83e5ac6747bbea4a85
Andreas Schmidt <andreas@schmidt.name>
2025-01-06 16:05:47 +0000

    add tests and fix bugs

--- companion_client/model/course_structure.py
+++ companion_client/model/course_structure.py
@@ -23,14 +23,14 @@ class CourseTopic(BaseModel):
 class Section(BaseModel):
     seqno: int
     seqno_padded: str
-    seqno_end: int
-    seqno_end_padded: str
+    seqno_end: int | None = None
+    seqno_end_padded: str | None = None
     seqnos: list[int]
 
     seqno_perm: int | None = None
     seqno_perm_end: int | None = None
 
-    title_short_de: int | None = None
+    title_short_de: str | None = None
     title_short_en: str | None = None
     title_long_de: str | None = None
     title_long_en: str | None = None
@@ -61,9 +61,9 @@ class CourseInstanceSlot(BaseModel, arbitrary_types_allowed=True):
 class CourseDescription(BaseModel):
     course_short: str
     course_long: str
-    description: str
+    description: str | None = None
     default_language: str
-    icon: str
+    icon: str | None = None
 
     proper_course: bool
 
@@ -75,58 +75,58 @@ class CourseDescription(BaseModel):
     show_qa: bool
     
     # Bot UI configuration
-    bot_intro: str
-    show_topic_suggestions: bool
-    max_topic_suggestions: int
-    topic_suggestions_placement: str
+    bot_intro: str | None = None
+    show_topic_suggestions: bool = True
+    max_topic_suggestions: int = 3
+    topic_suggestions_placement: str | None = "message"
 
-    show_question_suggestions: bool
-    max_question_suggestions: int
-    question_chips_placement: str
+    show_question_suggestions: bool = True
+    max_question_suggestions: int = 3
+    question_chips_placement: str  | None = "message"
 
-    show_thumbs_voting: bool
+    show_thumbs_voting: bool = True
 
-    show_similar_materials: bool
-    max_sources_display: int
+    show_similar_materials: bool = True
+    max_sources_display: int | None = 5
 
-    show_sources: bool
-    max_score_sources_display: int
+    show_sources: bool = True
+    max_score_sources_display: float | None = None
 
-    in_place_nav: bool
-    survey_link: str
+    in_place_nav: bool = True
+    survey_link: str | None = None
 
     # Agent
-    functions: list
-    use_agent: bool
+    functions: list = []
+    use_agent: bool = True
 
     # Bot backend configuraion
     max_sources_raw: int
     max_sources_context: int
-    max_score_sources_context: int
+    max_score_sources_context: float
     max_topic_suggestions_history: int
     max_question_suggestions_history: int
-    material_type_weight: int
+    material_type_weight: float
     history_ttl_minutes: int
     max_history: int
-    bot_temperature: int
-    reranking_strategy: str
-    vectordb: str
-    model_question: str
-    model_answer: str
-    model_topic: str
-    model_slot_query: str
-    source_template: str
-    document_group_intro_template: str
+    bot_temperature: float | None = None
+    reranking_strategy: str | None = None
+    vectordb: str | None = None
+    model_question: str | None = None
+    model_answer: str | None = None
+    model_topic: str | None = None
+    model_slot_query: str | None = None
+    source_template: str | None = None
+    document_group_intro_template: str | None = None
     group_sources: bool
     prompt_for_info: bool
     prompt_for_info_continue: bool
-    prompt_info_prompt: str
-    prompt_info_model: str
-    prompt_info_temperature: int
-    prompt_selection_strategy: str
-    prompt_selection_prompt: str
-    prompt_selection_model: str
-    prompt_selection_temperature: int
+    prompt_info_prompt: str | None = None
+    prompt_info_model: str | None = None
+    prompt_info_temperature: int | None = None
+    prompt_selection_strategy: str | None = None
+    prompt_selection_prompt: str | None = None
+    prompt_selection_model: str | None = None
+    prompt_selection_temperature: int | None = None
     condense_history: bool
     check_slot_prompt: bool
     rewrite_query: bool
